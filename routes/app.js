9888029de08e90527950108bda16eff49eeb2b7d
Ruijie Zhang <jerrydream123@gmail.com>
2016-07-04 19:18:42 -0700

    set up mongodb

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('node');
+router.get('/:usertitle?', function(req, res, next) {
+    res.render('node', {title: req.params.usertitle ?  req.params.usertitle : ''});
+});
+
+router.post('/', function(req, res, next) {
+	res.redirect('/' + req.body.title);
 });
 
 module.exports = router;
